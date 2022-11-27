d22460af4e21bda45c6daf0a46b49a0143b606ac
Utku Çelik <utkuce@gmail.com>
2022-11-27 18:23:17 -0500

    refactor

--- World/World.gd
+++ World/World.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-onready var player = $Player
-
 func _ready():
 	var screenSize = get_viewport().size
-	var playerX = screenSize.x/4
-	var playerY = screenSize.y/2
-	player.position = Vector2(playerX, playerY)
+	$Player.position = Vector2(screenSize.x/4, screenSize.y/2)
