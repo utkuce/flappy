e020c43190cda39380470de3d6fc48494214ef78
Utku Çelik <utkuce@gmail.com>
2022-11-27 17:36:01 -0500

    adjust: input variables

--- Player/PlayerControls.gd
+++ Player/PlayerControls.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export var pulse = 1000
+export var pulse = 750
 export var gravity = 25
 
 onready var debugInfo : CanvasLayer = get_node("/root/DebugInfo")
@@ -10,10 +10,10 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	var falling = velocity > -10
+	var falling = velocity > 0
 	if (falling):
 		if Input.is_action_pressed("primary_input"):
-			velocity -= pulse
+			velocity = -pulse
 		
 	velocity += gravity
 	position.y += velocity * delta
